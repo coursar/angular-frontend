1bc3184d8147f7f9138dbd1e420f7bf2dd8ce5f1
Student <student@rosbank.ru>
2021-10-03 21:20:57 +0300

    display one purchase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PurchaseDto } from './dto/purchase-dto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'intro';
+// Alt + Insert -> generation
+// Alt + Enter -> fix
+export class AppComponent implements OnInit {
+  item?: PurchaseDto;
+
+  ngOnInit(): void {
+    // инициализация первоначальной загрузки данных
+    //this.item = new PurchaseDto(1, 'Water', 100.99);
+  }
+
+  // 1. constructor
 }
---
+++ src/app/dto/purchase-dto.ts
@@ -0,0 +1,8 @@
+export class PurchaseDto {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number,
+  ) {
+  }
+}
