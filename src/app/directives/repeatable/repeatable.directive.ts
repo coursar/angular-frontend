6e620a91a9ece4bd3537a5a19c98fcb21f0f1180
Student <student@rosbank.ru>
2021-10-04 04:41:36 +0300

    differs

--- src/app/directives/repeatable/repeatable.directive.ts
+++ src/app/directives/repeatable/repeatable.directive.ts
@@ -1,23 +1,51 @@
-import { Directive, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Directive, DoCheck,
+  Input, IterableDiffer,
+  IterableDiffers,
+  OnChanges, OnInit,
+  SimpleChanges,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Directive({
   selector: '[appRepeatableOf]'
 })
-export class RepeatableDirective implements OnChanges {
+export class RepeatableDirective implements OnInit, DoCheck {
   @Input() appRepeatableOf: any[] = [];
+  private differ?: IterableDiffer<any>;
 
   constructor(
     private container: ViewContainerRef,
     private template: TemplateRef<any>,
+    private differs: IterableDiffers,
+    private detector: ChangeDetectorRef,
   ) { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.container.clear();
-    this.appRepeatableOf.forEach(o => {
+  ngOnInit() {
+    this.differ = this.differs.find(this.appRepeatableOf).create();
+  }
+
+  ngDoCheck(): void {
+    const changes = this.differ?.diff(this.appRepeatableOf);
+    if (changes === null) {
+      return;
+    }
+
+    changes?.forEachAddedItem(o => {
       this.container.createEmbeddedView(
         this.template,
-        {'$implicit': o}
-      )
+        {'$implicit': o.item},
+      );
+    });
+
+    // TODO: move
+
+    changes?.forEachRemovedItem(o => {
+      if (o.previousIndex !== null) {
+        this.container.remove(o.previousIndex);
+      }
     });
   }
 
