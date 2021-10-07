71f133cdeb15f25cdca727e5139b5ae980088197
coursar <coursar@gmail.com>
2021-10-07 11:06:59 +0300

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ComponentFactoryResolver,
-  ElementRef,
-  OnInit,
-  ViewChild,
-  ViewContainerRef
-} from '@angular/core';
-import {AComponent} from './components/a/a.component';
-import {BComponent} from './components/b/b.component';
-import {fromEvent, of} from "rxjs";
-import {catchError, debounceTime, filter, map, switchMap} from "rxjs/operators";
-import {ajax} from "rxjs/ajax";
-import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -21,43 +7,10 @@ import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 })
 // Alt + Insert -> generation
 // Alt + Enter -> fix
-export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild('container', {static: true, read: ViewContainerRef})
-  container?: ViewContainerRef;
-  @ViewChild('tplSearch', {static: true}) search?: ElementRef<HTMLInputElement>
-
+export class AppComponent implements OnInit {
   constructor(private resolver: ComponentFactoryResolver) {
   }
 
   ngOnInit(): void {
-  }
-
-  ngAfterViewInit(): void {
-    if (this.search) {
-      fromEvent(this.search.nativeElement, 'input')
-        .pipe(
-          debounceTime(100),
-          map(o => (o!.target as HTMLInputElement).value),
-          filter(o => o.trim() !== ''),
-          switchMap(
-            o => ajax(`http://localhost:8080/api/search?text=${o}`)
-              .pipe(
-                map(o => o.response),
-                catchError(err => of([]))
-              )
-          ),
-        )
-        .subscribe(evt => console.log(evt));
-    }
-  }
-
-  onAddComponentA() {
-    const factory = this.resolver.resolveComponentFactory(AComponent);
-    this.container?.createComponent(factory);
-  }
-
-  onAddComponentB() {
-    const factory = this.resolver.resolveComponentFactory(BComponent);
-    this.container?.createComponent(factory);
   }
 }
